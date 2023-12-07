2a57aaa0634d3173b9c2ec634652bcd08ca399a1
thomasc21 <80321973+thomasc21@users.noreply.github.com>
2023-12-07 09:59:09 +0100

    ajout de la requete sur le back
    Attention les erreurs ne son pas gérer ( si il y a pas de mdp ou email )

--- AWIFrontEnd/src/pages/common/Login.js
+++ AWIFrontEnd/src/pages/common/Login.js
@@ -10,6 +10,33 @@ const Login = () => {
 
   const handleLoginSubmit = (formData) => {
     // Traitement spécifique à la page de connexion avec les données du formulaire
+    const apiURL ="http://localhost:3000/authentication-module/signin";
+    var idusers = formData.email;
+    var password = formData.password;
+    console.log(idusers);
+    console.log(password);
+    try{
+      fetch(apiURL,{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'Content-Length':'208',
+          'X-Powered-By': 'Express',
+          'Date': Date.now(),
+          'Connection': 'keep-alive',
+          'Keep-Alive': 'timeout=5', 
+          'ETag':'W/"d0-ksiszyPX7J6Y3qdwtiOUEUkBGIE"'
+        },
+        body: JSON.stringify({"Email": idusers, "Password": password})
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+    }
+    catch(error){
+      console.log(error);
+    }
     console.log('Données du formulaire de connexion :', formData);
     // Ajoutez votre logique de connexion ici
   };
@@ -27,7 +54,7 @@ const Login = () => {
         <h2 id ="insc">Se connecter</h2>
         <Form  fields={loginFields}
         buttonText="Se connecter"
-        onSubmit={handleLoginSubmit}
+        onSubmit={handleLoginSubmit }
         clickableText="S'inscrire"
         clickableHref="/register"
       />
