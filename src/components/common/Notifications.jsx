f8fa79d2a9f1a5f1ea903f961d0964d58a779733
Marine Cantaloube Girona <88738724+MaliceCg@users.noreply.github.com>
2024-01-20 14:03:16 +0100

    Notif

---
+++ src/components/common/Notifications.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import styles from '../../styles/notif.module.css';
+
+const Notifications = () => {
+const [listNotif, setListNotif] = useState([]);
+
+useEffect(() => {
+    const fetchNotif = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/notif-module');
+        if (response.ok) {
+        const data = await response.json();
+          setListNotif(data); // Mettre à jour l'état avec les données récupérées
+        }   else {
+        throw new Error('Erreur lors de la récupération des données');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+    };
+
+    fetchNotif(); // Appel de la fonction pour récupérer les données au chargement du composant
+  }, []); // Le tableau vide [] signifie que useEffect ne s'exécute qu'une seule fois au montage du composant
+
+return (
+    <div>
+        <div className={styles.NotificationContainer}>
+        <h1>Notifications</h1>
+        {listNotif.map((notif, index) => (
+            <div key={index}>
+                <div>
+                    {notif.NomNotification}
+                </div>
+            </div>
+        ))}
+        </div>
+    </div>
+
+);
+};
+
+export default Notifications;
--- src/pages/admin/AdminNotifications.jsx
+++ src/pages/admin/AdminNotifications.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/common/Header';
 import NavbarAdmin from '../../components/common/NavbarAdmin';
+import styles from '../../styles/notif.module.css';
 
 
 const AdminNotifications = () => {
   return (
     <div>
         <Header currentPage="notifications" />
+        <Link to='/notif'>
+        <button className={styles.btnSend}>Envoyez une notification à tous les bénévoles </button>
+        </Link>
         <NavbarAdmin />
     </div>
   );
