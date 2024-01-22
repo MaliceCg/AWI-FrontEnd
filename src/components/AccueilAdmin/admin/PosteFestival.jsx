a46dc6b0fb1b39cb06c5c2c066870935b95173fd
Marine Cantaloube Girona <88738724+MaliceCg@users.noreply.github.com>
2024-01-22 18:59:31 +0100

    crudPost

--- src/components/AccueilAdmin/admin/PosteFestival.jsx
+++ src/components/AccueilAdmin/admin/PosteFestival.jsx
@@ -1,10 +1,16 @@
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from 'react-router-dom';
 import styles from "../../../styles/espaceCreation.module.css";
 
 const PosteFestival = (props) => {
     const { poste } = props;
+    const { idFestival } = useParams();
+    const accessToken = localStorage.getItem('token');
+    const idPoste = poste.idPoste;
+    console.log(idPoste);
+    console.log(accessToken);
     let backgroundColor = '';
 
     switch (poste.nomPoste.toLowerCase()) {
@@ -21,26 +27,157 @@ const PosteFestival = (props) => {
             backgroundColor = '#105C9F';
             break;
         default:
+            backgroundColor = '#F4B740';
             break;
     }
 
     const dynamicStyle = {
         backgroundColor: backgroundColor,
     };
+    const [isConfirmationDialogOpen, setConfirmationDialogOpen] = useState(false);
+    const [isEditPopupOpen, setEditPopupOpen] = useState(false);
+    const [editedPoste, setEditedPoste] = useState({
+        nomPoste: poste.nomPoste,
+        description: poste.description,
+        capacite: poste.capacite,
+    });
+
+    const openConfirmationDialog = () => {
+        setConfirmationDialogOpen(true);
+    };
+
+    const closeConfirmationDialog = () => {
+        setConfirmationDialogOpen(false);
+    };
+
+    const openEditPopup = () => {
+        setEditPopupOpen(true);
+    };
+
+    const closeEditPopup = () => {
+        setEditPopupOpen(false);
+    };
+    const updatePoste = () => {
+        fetch(`http://localhost:3000/position-module/${idPoste}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify(editedPoste),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur lors de la mise à jour du poste');
+            }
+            // Mettre à jour les données du poste dans l'interface utilisateur si nécessaire
+            // Peut-être recharger les données depuis le serveur ou mettre à jour l'état local
+        })
+        .catch(error => {
+            alert(error.message);
+        });
+        alert('Le poste a bien été modifié');
+        // Après la mise à jour, fermer la popup de modification
+        closeEditPopup();
+    };
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        const numericValue = name === "capacite" ? parseInt(value, 10) : value;
+        setEditedPoste(prevState => ({
+            ...prevState,
+            [name]: numericValue,
+        }));
+    };
+
+
+
+
+    const deletePoste = () => {
+        fetch(`http://localhost:3000/employer-module/${idFestival}/${idPoste}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+
+      }).then(response => {
+             if (!response.ok) {
+                throw new Error('Erreur lors de la suppression de la relation employer');
+           }
+             return fetch(`http://localhost:3000/position-module/${idPoste}`, {
+                method: 'DELETE',
+                headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    }
+            });
+         }).then(response => {
+             if (!response.ok) {
+               throw new Error('Erreur lors de la suppression du poste');
+           }
+
+       
+        }).catch(error => {
+            alert(error.message);
+        });
+        alert('Le poste a bien été supprimé');
+        // Après la suppression, fermer la boîte de dialogue
+        closeConfirmationDialog();
+    };
+
+
 
     return (
         <div className={`${styles.posteBox}`} style={dynamicStyle}>
-           <h3 className={styles.posteTitle}>{poste.nomPoste}</h3>
-           <div>
-                <button className={styles.posteButton}>
+            <h3 className={styles.posteTitle}>{poste.nomPoste}</h3>
+            <div>
+                <button className={styles.posteButton} onClick={openEditPopup}>
                     <EditIcon />
                 </button>
 
-                <button className={styles.posteButton}>
+                <button className={styles.posteButton} onClick={openConfirmationDialog}>
                     <DeleteForeverIcon />
                 </button>
+
+                {isConfirmationDialogOpen && (
+                    <div className={styles.confirmationDialog}>
+                        <div className={styles.confDialog}>
+                        <p className={styles.TxtConf}>Êtes-vous sûr de vouloir supprimer ce poste ?</p>
+                        <button className={styles.btn2} onClick={deletePoste}>Oui</button>
+                        <button className={styles.btn2} onClick={closeConfirmationDialog}>Annuler</button>
+                        </div>
+                    </div>
+                )}
             </div>
-        </div>
+
+                {isEditPopupOpen && (
+                    <div className={styles.editPopup}>
+                        <div className={styles.editPopupContent}>
+                        <h1 className={styles.titre}>Modification du poste :</h1>
+                        <label className={styles.label}>Nom du poste</label>
+                        <input className={styles.editInput}
+                            type="text"
+                            name="nomPoste"
+                            value={editedPoste.nomPoste}
+                            onChange={handleInputChange}
+                        />
+                        <label className={styles.label}>Description</label>
+                        <textarea className={styles.editText}
+                            name="description"
+                            value={editedPoste.description}
+                            onChange={handleInputChange}
+                        ></textarea>
+                        <label className={styles.label}>Places disponibles </label>
+                        <input className={styles.editInput}
+                            type="number"
+                            name="capacite"
+                            value={editedPoste.capacite}
+                            onChange={handleInputChange}
+                        />
+                        <button className={styles.btn2} onClick={updatePoste}>Enregistrer</button>
+                        <button className={styles.btn2} onClick={closeEditPopup}>Annuler</button>
+                        </div>
+                    </div>
+                )}
+            </div>
     );
 };
 
--- src/pages/admin/AdminCreation.jsx
+++ src/pages/admin/AdminCreation.jsx
@@ -1,12 +1,13 @@
+import AddIcon from '@mui/icons-material/Add';
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import PosteFestival from '../../components/AccueilAdmin/admin/PosteFestival';
 import Header from '../../components/common/Header';
 import NavbarAdmin from '../../components/common/NavbarAdmin';
-import PosteFestival from '../../components/AccueilAdmin/admin/PosteFestival';
 import styles from '../../styles/espaceCreation.module.css';
-import { useParams } from 'react-router-dom';
-import AddIcon from '@mui/icons-material/Add';
 
 const EspaceCreation = () => {
+  const accessToken = localStorage.getItem('token');
   const { idFestival } = useParams();
   const [postes, setPostes] = useState([]);
 
@@ -41,6 +42,85 @@ const EspaceCreation = () => {
 
   console.log("postes : ", postes);
 
+  const openAddPostePopup = () => {
+    // Réinitialisez l'état pour un nouveau poste
+    setNewPoste({
+      nomPoste: "",
+      description: "",
+      capacite: 0,
+    });
+    setAddPostePopupOpen(true);
+  };
+
+  const closeAddPostePopup = () => {
+    setAddPostePopupOpen(false);
+  };
+
+  const [isAddPostePopupOpen, setAddPostePopupOpen] = useState(false);
+  const [newPoste, setNewPoste] = useState({
+    nomPoste: "",
+    description: "",
+    capacite: 0,
+  });
+
+const addPoste = async () => {
+console.log("newPoste : ", newPoste);
+console.log(JSON.stringify(newPoste));
+    const response = await fetch('http://localhost:3000/position-module', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+              },
+      body: JSON.stringify(newPoste),
+      });
+
+    if (response.ok) {
+      const posteResponseData = await response.json(); // Convertir la réponse en JSON
+      console.log(posteResponseData);
+      const posteId = posteResponseData.idPoste; // Récupérer l'ID du poste créé
+      console.log(posteId);
+      console.log(idFestival);
+      // Lier le poste au festival dans la table employer
+      const employerData = {
+        idFestival: parseInt(idFestival, 10),
+        idPoste: posteId,
+      };
+      console.log(employerData);
+      const employerResponse = await fetch('http://localhost:3000/employer-module', {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': `Bearer ${accessToken}`,
+                    },
+                    body: JSON.stringify(employerData),
+                  });
+      if (employerResponse.ok) {
+        console.log(`Le poste ${newPoste.nomPoste} a été lié au festival avec succès !`);
+        setAddPostePopupOpen(false);
+      }
+      else {
+        console.error(`Erreur lors de la liaison du poste ${newPoste.nomPoste} au festival.`);
+      }
+        }
+        else {
+          console.log(response);
+          console.error(`Erreur lors de la création du poste ${newPoste.nomPoste}.`);
+        }
+      }
+
+const handleNewPosteInputChange = (e) => {
+  const { name, value } = e.target;
+  
+  // Convertir la valeur en nombre si le champ est "capacite"
+  const numericValue = name === "capacite" ? parseInt(value, 10) : value;
+
+  setNewPoste((prevPoste) => ({
+    ...prevPoste,
+    [name]: numericValue,
+  }));
+};
+  
   return (
     <div>
       <Header currentPage="espace-creation" />
@@ -59,7 +139,39 @@ const EspaceCreation = () => {
               ))}
             </div>
 
-            <button className={styles.addPoste}><AddIcon />Ajouter un poste</button>
+            <button className={styles.addPoste} onClick={openAddPostePopup}>
+              <AddIcon /> Ajouter un poste
+            </button>
+
+            {isAddPostePopupOpen && (
+              <div className={styles.editPopup}>
+              <div className={styles.editPopupContent}>
+                <h1 className={styles.titre}>Ajouter un poste :</h1>
+                <label className={styles.label} >Nom du poste</label>
+                <input className={styles.editInput}
+                  type="text"
+                  name="nomPoste"
+                  value={newPoste.nomPoste}
+                  onChange={handleNewPosteInputChange}
+                />
+                <label className={styles.label}>Description</label>
+                <textarea
+                  name="description"
+                  value={newPoste.description}
+                  onChange={handleNewPosteInputChange}
+                ></textarea>
+                <label className={styles.label}>Places disponibles </label>
+                <input className={styles.editInput}
+                  type="number"
+                  name="capacite"
+                  value={newPoste.capacite}
+                  onChange={handleNewPosteInputChange}
+                />
+                <button className={styles.btn2} onClick={addPoste}>Ajouter</button>
+                <button className={styles.btn2} onClick={closeAddPostePopup}>Annuler</button>
+              </div>
+            </div>
+            )}
           </div>
         </div>
       </div>
