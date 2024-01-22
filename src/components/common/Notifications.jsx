3461f53f2b0ab4c5dd9c1ffd8e27336ff00e7c0e
Marine Cantaloube Girona <88738724+MaliceCg@users.noreply.github.com>
2024-01-22 16:38:24 +0100

    getNotif fini

--- src/components/common/Notifications.jsx
+++ src/components/common/Notifications.jsx
@@ -2,42 +2,57 @@ import React, { useEffect, useState } from 'react';
 import styles from '../../styles/notif.module.css';
 
 const Notifications = () => {
-const [listNotif, setListNotif] = useState([]);
+  const [listNotif, setListNotif] = useState([]);
 
-useEffect(() => {
+  useEffect(() => {
     const fetchNotif = async () => {
-    try {
+      try {
         const response = await fetch('http://localhost:3000/notif-module');
         if (response.ok) {
-        const data = await response.json();
-        console.log(data);
+          const data = await response.json();
+          console.log(data);
           setListNotif(data); // Mettre à jour l'état avec les données récupérées
-        }   else {
-        throw new Error('Erreur lors de la récupération des données');
+        } else {
+          throw new Error('Erreur lors de la récupération des données');
         }
-    } catch (error) {
+      } catch (error) {
         console.error(error);
-    }
+      }
     };
 
     fetchNotif(); // Appel de la fonction pour récupérer les données au chargement du composant
   }, []); // Le tableau vide [] signifie que useEffect ne s'exécute qu'une seule fois au montage du composant
 
-return (
-    <div>
-        <div className={styles.NotificationContainer}>
-        <h1>Notifications</h1>
-        {listNotif.map((notif, index) => (
-            <div key={index}>
-                <div>
-                    {notif.TexteNotification}
-                </div>
-            </div>
-        ))}
-        </div>
-    </div>
+  // Séparer les notifications en deux groupes en fonction du type
+  const soiréeDécouverteNotifs = listNotif.filter((notif) => notif.Type === 'Soirée Découverte');
+  const autresNotifs = listNotif.filter((notif) => notif.Type !== 'Soirée Découverte');
 
-);
+  return (
+    <div>
+      <div className={styles.NotificationContainer}>
+        {soiréeDécouverteNotifs.length > 0 && (
+          <>
+            <h1>Soirée Découverte</h1>
+            {soiréeDécouverteNotifs.map((notif, index) => (
+              <div key={index}>
+                <div className={styles.TexteNotification}>{notif.TexteNotification}</div>
+              </div>
+            ))}
+          </>
+        )}
+        <h1>Notifications</h1>
+        {autresNotifs.length > 0 ? (
+          autresNotifs.map((notif, index) => (
+            <div key={index}>
+              <div className={styles.TexteNotification}>{notif.TexteNotification}</div>
+            </div>
+          ))
+        ) : (
+          <p>Aucune notification</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Notifications;
