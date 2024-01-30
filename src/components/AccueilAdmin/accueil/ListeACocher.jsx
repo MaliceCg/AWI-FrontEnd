03daed9684ec9a841fac62fa1d7284f0ec5bd06d
thomasc21 <80321973+thomasc21@users.noreply.github.com>
2024-01-30 15:29:49 +0100

    add liste benevole

---
+++ src/components/AccueilAdmin/accueil/ListeACocher.jsx
@@ -0,0 +1,82 @@
+import Email from '@mui/icons-material/Email';
+import Calendar from '@mui/icons-material/CalendarMonth';
+import styles from '../../../styles/listeBenevole.module.css';
+import React, { useEffect, useState } from 'react';
+
+const GoCalendar = (id) => {
+  // Todo: Utiliser le backend pour accéder au calendrier
+}
+
+const ListeACocher = ({ benevole, index }) => {
+  const [checkboxChecked, setCheckboxChecked] = useState(benevole.isPresent);
+  const [nom, setNom] = useState('');
+  const [prenom, setPrenom] = useState('');
+  const [email, setEmail] = useState('');
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/authentication-module/${benevole.idBenevole}`);
+        if (response.ok) {
+          const data = await response.json();
+          setNom(data.Nom);
+          setPrenom(data.Prenom);
+          setEmail(data.Email);
+        } else {
+          throw new Error('Erreur lors de la récupération des données');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [benevole.idBenevole]);
+
+  const handleCheckboxChange = async () => {
+    try {
+      // Make API call to update presence on the server
+      console.log("changement de présence ")
+      const response = await fetch(`http://localhost:3000/inscription-module/inscriptionPresent/${benevole.idBenevole}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ Presence:!checkboxChecked }),
+      });
+
+      if (response.ok) {
+        // Update local state only if the server update was successful
+        setCheckboxChecked(!checkboxChecked);
+      } else {
+        throw new Error('Erreur lors de la mise à jour de la présence sur le serveur');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
+  return (
+    <div>
+      <div className={styles.benevoleBox} key={index}>
+        <div>
+          <div className={styles.nomBenevole}>
+            {nom} {prenom}
+          </div>
+          {benevole.Pseudo}
+        </div>
+        <div className={styles.iconsBenevole}>
+        <a href={`mailto:${email}`} target="_blank" rel="noopener noreferrer">
+            <Email />
+          </a>
+          <Calendar onClick={() => GoCalendar(benevole.idBenevole)} />
+          <input type="checkbox" checked={checkboxChecked} onChange={handleCheckboxChange} />  
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ListeACocher;
--- src/pages/accueil/ListeBenevole.jsx
+++ src/pages/accueil/ListeBenevole.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styles from '../../styles/listeBenevole.module.css';
+import ListeACocher from '../../components/AccueilAdmin/accueil/ListeACocher.jsx';
+
 import Header from '../../components/common/Header';
-import Navbar from '../../components/common/Navbar';
+import NavbarAdmin from '../../components/common/NavbarAdmin';
+
+
 
 
 const ListeBenevole = () => {
+  const [listBenevoles, setListBenevoles] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/inscription-module/getAllbenevoleInscritToday');
+        if (response.ok) {
+          const data = await response.json();
+          setListBenevoles(data);
+        } else {
+          throw new Error('Erreur lors de la récupération des données');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  
+  console.log(listBenevoles);
+
   return (
     <div>
-        <Header currentPage="liste-benevole" />
-        <Navbar />
+      <Header currentPage="liste-benevole" />
+      <div className={styles.wrapper}>
+        <div className={styles.navbar}>
+          <NavbarAdmin />
+        </div>
+        <div className={styles.benevoleContainer}>
+          {listBenevoles.map((benevole, index) => (
+            <ListeACocher benevole={benevole} key={index}  />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
