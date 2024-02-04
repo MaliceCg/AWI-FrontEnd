eda04986d71b870a74f2c8f5f010ef58e35333c6
thomasc21 <80321973+thomasc21@users.noreply.github.com>
2024-02-04 23:02:44 +0100

    en cours

--- src/utils/Verificateur.js
+++ src/utils/Verificateur.js
@@ -3,38 +3,38 @@ import Unauthorized from "../pages/common/Unauthorized";
 const accessToken = localStorage.getItem('token');
 
 const Verificateur = ({ roleAutorise, composant }) => {
-  const [authorize, setAuthorize] = useState(false);
+  const [authorize, setAuthorize] = useState("");
   console.log(roleAutorise);
   console.log(accessToken);
+  const roleAutoriseSplit = roleAutorise.split(',');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:3000/authentication-module/authorized',{ // Remplacez avec votre logique pour vérifier le token
-          method: 'Get',
+        const response = await fetch('http://localhost:3000/authentication-module/authorized',{
+          method: 'GET',
           headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${accessToken}`,
           },
-          body: {
-            "RoleAuthorized" : roleAutorise,
-            "Token" : accessToken,
-          }
         });
         if (response.ok) {
           const data = await response.json();
+          console.log("data",data)
           setAuthorize(data); // Mettez à jour l'autorisation
         } else {
           throw new Error('Erreur lors de la vérification du token ');
         }
       } catch (error) {
-        console.log(error);
+        console.log("error" + error);
       }
     };
     fetchData();
   }, [roleAutorise]);
-
-  if (authorize) {
+  console.log(authorize);
+  if (authorize in roleAutoriseSplit) {
+    composant = composant;
+  }else{
     composant = <Unauthorized />;
   }
   return composant;
